4639c5b1d1a8aa351cab3505577e49b7f6099fed
chaebeen <tnwl05200@gmail.com>
2025-11-28 18:50:55 +0900

    Edit

--- start.js
+++ start.js
@@ -1,3 +1,4 @@
 document.getElementById("start-btn").addEventListener("click", function () {
-  window.location.href = "cardgame.html"; // 이동할 페이지 주소
+  window.location.href = "cardgame.html";
+  //이동할 페이지 주소
 });
