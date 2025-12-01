a2d0c3db97415b5cf21538bf1d303e2feb0e2bee
chaebeen <tnwl05200@gmail.com>
2025-12-01 16:55:57 +0900

    add

--- start.js
+++ start.js
@@ -1,3 +1,3 @@
-document.getElementById("start-btn").addEventListener("click", function () {
+document.getElementById("btn").addEventListener("click", function () {
   window.location.href = "cardgame.html";
 });
